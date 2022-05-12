43779feccd9468fa908bf93b58521c5d1f177ad4
chhaya abhay singh <singhchaya269@gmail.com>
2022-05-13 01:36:45 +0530

    inheritance

---
+++ Inheritance/Inheritance/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+class Entity
+{
+public:
+	float X, Y;
+
+	void Move(float xa, float ya)
+	{
+		X += xa;
+		Y += ya;
+	}
+};
+
+class Player :public Entity
+{
+public :
+	const char* Name;
+	float X, Y;
+
+	void Move(float xa, float ya)
+	{
+		X += xa;
+		Y += ya;
+	}
+
+	void PrintName()
+	{
+		std::cout << Name << std::endl;
+	}
+};
+
+int main()
+{  
+	std::cout << sizeof(Entity) << std::endl;
+	std::cout << sizeof(Player)<<std::endl;
+	Player player;
+	player.Move(5, 5);
+	player.X = 2;
+
+
+              
+	std::cin.get();
+}
