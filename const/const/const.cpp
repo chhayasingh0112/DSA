6d9734e714b245f5b72e222463d7f455bd670057
chhaya abhay singh <singhchaya269@gmail.com>
2022-05-22 13:08:29 +0530

    git class pointer

--- const/const/const.cpp
+++ const/const/const.cpp
@@ -1,8 +1,31 @@
 #include<iostream>
 #include<string>
 
+class Entity
+{
+private:
+	int m_X, m_Y;
+	mutable int var;
+public:
+	int GetX() const
+	{
+		return m_X;
+	}
+
+	int SetX(int x)
+	{
+		m_X = x;
+	}
+};
+
+void PrintEntity(const Entity& e)
+{
+	std::cout << e.GetX() << std::endl;
+}
+
 int main()
 {
+	Entity e;
 	const int MAX_AGE = 90;
 
 	const int* a = new int;
