bf231f50e32761cbcfa67693b50fbc5f40b7738f
chhaya abhay singh <singhchaya269@gmail.com>
2022-05-22 15:44:19 +0530

    Created an Entity \

--- Constructor Initializer List/Constructor Initializer List/list.cpp
+++ Constructor Initializer List/Constructor Initializer List/list.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
 #include<string>
 
+class Example
+{
+public:
+	Example()
+	{
+		std::cout << "Created Entity!" << std::endl;
+
+	}
+
+	Example(int x)
+	{
+		std::cout << "Created Entity With " << x << "!" << std::endl;
+	}
+};
+
 
 class Entity
 {
 private:
 	std::string m_Name;
-	
+	Example m_Example;
 public:
 	Entity()
-		: m_Name("Unknown")
 	{
+		m_Name = std::string("Unknown");
+		m_Example = Example(1);
 	}
 	Entity(const std::string& name)
 		: m_Name(name)
@@ -24,10 +40,7 @@ public:
 int main()
 {
 	Entity e0;
-	std::cout << e0.GetName() << std::endl;
-
-	Entity e1("Sanika");
-	std::cout << e1.GetName() << std::endl;
+	
 
 	std::cin.get();
 
