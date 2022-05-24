02743efb095fedb6c8fb31f508128f51cd6ba852
chhaya abhay singh <singhchaya269@gmail.com>
2022-05-24 22:17:59 +0530

    New operator

--- New Keyword/New Keyword/Source.cpp
+++ New Keyword/New Keyword/Source.cpp
@@ -1,17 +1,24 @@
-#include <stdio.h>
+#include<iostream>
+#include <string>
+
+using String = std::string;
+
+class Entity
+{
+private:
+	String m_Name;
+public:
+	Entity() :m_Name("Unknown") {}
+	Entity(const String& name) : m_Name(name) {}
+
+	const String& GetName() const { return m_Name; }
+};
 
 int main()
-
 {
-
-    int k;
-
-    {
-
-        int k;
-
-        for (k = 0; k < 10; k++);
-
-    }
-
+	int a = 2;
+	int* b = new int[50];
+	Entity* e = new Entity();
+	std::cin.get();
+	delete e;
 }
