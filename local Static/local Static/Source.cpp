71a0caffd3524527c0551971c9349c06cf959f28
chhaya abhay singh <singhchaya269@gmail.com>
2022-06-03 13:04:32 +0530

    Local Satic

---
+++ local Static/local Static/Source.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+void Function()
+{
+	static int i = 0;
+	i++;
+	std::cout << i << std::endl;
+
+}
+
+int main()
+{
+	Function();
+	Function();
+	Function();
+	Function();
+	Function();
+	 
+	std::cin.get();
+}
