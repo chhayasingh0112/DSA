9d5ac635302e932d4697c6fd31603b00b10bf0d0
chhaya abhay singh <singhchaya269@gmail.com>
2022-06-04 21:45:06 +0530

    Template

---
+++ Templates/Templates/Templet.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+template<typename T>
+void Print(T value)
+{
+	std::cout << value << std::endl;
+
+}
+
+int main()
+{
+	Print(5);
+	Print("Hello");
+	Print(5.5f);
+
+	std::cin.get();
+}
