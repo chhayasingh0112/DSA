900bb2a69f2c8582c098ace6fb96fbad3bd62a6b
chhaya abhay singh <singhchaya269@gmail.com>
2022-06-04 23:28:18 +0530

    Template p2

--- Templates/Templates/Templet.cpp
+++ Templates/Templates/Templet.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <string>
 
-template<typename T>
-void Print(T value)
+template<typename T,int N>
+class Array
 {
-	std::cout << value << std::endl;
-
-}
+private:
+	T m_Array[N];
+public:
+	int GetSize() const { return N; }
+};
 
 int main()
 {
-	Print(5);
-	Print("Hello");
-	Print(5.5f);
+	Array<std::string,50> array;
+
+	std::cout << array.GetSize() << std::endl;
 
 	std::cin.get();
 }
