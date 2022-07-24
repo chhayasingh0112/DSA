0e5ec4d04e44f377878b2d5c7c5d2cb235771279
chhaya abhay singh <singhchaya269@gmail.com>
2022-07-24 17:09:47 +0530

    Linnear Search

--- Tower of Hanoi/Tower of Hanoi/toh.cpp
+++ Tower of Hanoi/Tower of Hanoi/toh.cpp
@@ -1,15 +1,31 @@
 #include <stdio.h>
-void TOH(int n, int A, int B, int C)
+//void TOH(int n, int A, int B, int C)
+//{
+//	if (n > 0)
+//	{
+//		TOH(n - 1, A, C, B);
+//		printf("(%d,%d)\n", A, C);
+//		TOH(n - 1, B, A, C);
+//	}
+//}
+//int main()
+//{
+//	TOH(7, 1, 2, 3);
+//	return 0;
+//}
+
+int f(int n)
+
 {
-	if (n > 0)
-	{
-		TOH(n - 1, A, C, B);
-		printf("(%d,%d)\n", A, C);
-		TOH(n - 1, B, A, C);
-	}
-}
-int main()
-{
-	TOH(7, 1, 2, 3);
-	return 0;
+
+	static int i = 1;
+
+		if (n >= 5)return n;
+
+	n = n + i;
+
+	i++;
+
+	return f(n);
+
 }
